4f5fe6b0324f4cd70c6fa369492bc60d29ce4d23
Himasri451 <himasri451@gmail.com>
2026-04-20 01:00:00 +0530

    Create inference.py

---
+++ inference.py
@@ -0,0 +1,12 @@
+def run_inference():
+    print("Running AI detection...")
+
+    # Step 1: Load model (later we add YOLO here)
+    
+    # Step 2: Capture video / input
+    
+    # Step 3: Detect objects (students, phone)
+    
+    # Step 4: Apply cheating logic
+    
+    print("Detection complete")
